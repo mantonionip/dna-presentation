42c3f63672d400e2771fd8e4b59f9936a86e5e57
Mehdi Pilehvarian <mehdi.pilehvarian@gmail.com>
2020-08-09 18:52:46 -0400

    responsive added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
+const choices = Array.from(document.getElementsByClassName("question__choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
